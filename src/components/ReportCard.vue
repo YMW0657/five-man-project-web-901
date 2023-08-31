f53d15521a74414b7fabc299877dd9f98ac2c144
YMW0657 <ywan0657@student.monash.edu>
2023-09-01 00:53:21 +1000

    901

---
+++ src/components/ReportCard.vue
@@ -0,0 +1,44 @@
+<!-- 
+Component name: ReportCard
+use: to display information on the organization in the report page.
+props: dataprop {logo, org_name, description, link}
+-->
+
+<template>
+   <div color="cyan-lighten-5">
+        <div>
+            <a-row class="align-center justify-center">
+                <a-col xs="12" sm="12" md="5" lg="3">
+                    <a-avatar
+                    class="ma-4"
+                    size="200"
+                    rounded="0">
+                        <v-img :src="dataprop.logo"></v-img>
+                    </a-avatar>
+                </a-col>
+                <a-col xs="12" sm="12" md="7" lg="9">
+                    <div>
+                        {{dataprop.org_name}}
+                    </div>
+                    <div>
+                        {{dataprop.description}}
+                    </div>
+                    <a :href="dataprop.link" target="_blank">
+                        <v-btn  class="ms-2" color="success" on-hover>
+                            visit site
+                        </v-btn>
+                    </a> 
+                </a-col>
+            </a-row>
+        </div>
+    </div> 
+</template>
+
+<script setup>
+    const props = defineProps({
+        dataprop:{
+            type:Object
+        }
+    })
+
+</script>
